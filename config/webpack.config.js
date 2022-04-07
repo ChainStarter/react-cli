24ff6e0c9c09c22e0e00c0706f95a51374e5fbed
yuan30 <yuan30>
2022-04-07 10:02:21 +0800

    package fix

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -695,7 +695,12 @@ module.exports = function (webpackEnv) {
       //       },
       //     },
       //   }),
-      new HardSourceWebpackPlugin()
+      new HardSourceWebpackPlugin(),
+      new HardSourceWebpackPlugin.ExcludeModulePlugin([
+        {
+          test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+        }
+      ])
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
